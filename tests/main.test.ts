62bbe79b06fe94fa6f11dbd9f1bd7d02cbaa3638
winvdaking <pro.dorianlopez@gmail.com>
2022-10-17 15:30:15 +0200

    fix(#winv): test in comment bcs main function removed

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,8 +1,9 @@
 import { describe, expect, test } from '@jest/globals';
 import main from "../src/main";
-
+/*
 describe('main function', () => {
     test('main function returns the string "Hello, World!"', () => {
         expect(main()).toMatch("Hello, World!");
     })
 });
+*/
