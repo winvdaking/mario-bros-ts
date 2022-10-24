5989f6117c756a5fffca25ab8d1ebd96a2d64398
winvdaking <pro.dorianlopez@gmail.com>
2022-10-24 16:16:02 +0200

    feat(lexique): add CTRL C to exit

--- src/modules/elementWorld.ts
+++ src/modules/elementWorld.ts
@@ -12,7 +12,8 @@ class ElementWorld {
 		= : Représente un bloc
 		? : Représente un bloc magique avec un item (en dev)
 		<- : Pour que Mario marche vers la gauche
-		-> : Pour que Mario marche vers la droite`;
+		-> : Pour que Mario marche vers la droite
+		CTRL+C : Pour arrêter la partie`;
 	}
 };
 
